46c448f22a00acb5ce9cb4ceb89739780b9a8324
cyntachs <edrendacaymat@gmail.com>
2025-12-04 14:21:11 -0500

    add rebuild-venv

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 cd /
 
+# initialize folder
 if [ ! -f "/stable-diffusion/.installed" ]; then
     echo "Installing ComfyUI..."
     git clone https://github.com/comfyanonymous/ComfyUI.git /stable-diffusion
@@ -23,32 +24,49 @@ elif [ ! -f "/stable-diffusion/custom_nodes/ComfyUI-Manager/__init__.py" ]; then
     git pull
     pip3 install -r requirements.txt
 fi;
+
+# rebuild venv
+if [ -f "/stable-diffusion/.rebuild-venv" ]; then
+    rm -rf /venv/*
+    
+    python3 -m venv /venv
+    pip3 install tzdata opencv-python glcontext
+    pip3 install torch torchvision torchaudio pillow tqdm xformers --index-url https://download.pytorch.org/whl/cu130
+    pip3 install opencv-python-headless
+    pip3 install -r https://raw.githubusercontent.com/loscrossos/crossOS_acceleritor/refs/heads/main/acceleritor_torch280cu129_full.txt
+    pip3 install -r https://raw.githubusercontent.com/comfyanonymous/ComfyUI/master/requirements.txt
+    
+    rm -f /stable-diffusion/.rebuild-venv
+
+    touch /stable-diffusion/.sage-installed
+    touch /stable-diffusion/.use-sage
+fi;
+
+# install sage if not installed
 if [ ! -f "/stable-diffusion/.sage-installed" ]; then
     cd /stable-diffusion
     
-	#rm -f ./acceleritor_torch280cu129_full.txt
-    #wget https://raw.githubusercontent.com/loscrossos/crossOS_acceleritor/refs/heads/main/acceleritor_torch280cu129_full.txt
     pip3 install -r https://raw.githubusercontent.com/loscrossos/crossOS_acceleritor/refs/heads/main/acceleritor_torch280cu129_full.txt
     pip3 install -r requirements.txt
     
     cd /
     
     touch /stable-diffusion/.sage-installed
+    touch /stable-diffusion/.use-sage
 fi;
+
+# reinstall requirements
 if [ -f "/stable-diffusion/.req-reinstall" ]; then
     cd /stable-diffusion
     
     pip3 install -r requirements.txt
-	
-    # comfyui-manager is better at fixing dependencies
-	#find ./custom_nodes/ -name "requirements*.txt" -type f | while read -r file; do
-	#	pip3 install --upgrade-strategy only-if-needed -r "$file" 
-	#done
     
     cd /
     
     rm -f /stable-diffusion/.req-reinstall
 fi;
+
+# reinstall requirements (using online requirements)
 if [ -f "/stable-diffusion/.req-reinstall-online" ]; then
     cd /stable-diffusion
     
@@ -56,11 +74,12 @@ if [ -f "/stable-diffusion/.req-reinstall-online" ]; then
     
     cd /
     
-    rm -f /stable-diffusion/.req-reinstall
+    rm -f /stable-diffusion/.req-reinstall-online
 fi;
 
+# start comfyui
 cd /stable-diffusion
-if [ -f "/stable-diffusion/.sage-installed" ]; then
+if [ -f "/stable-diffusion/.use-sage" ]; then
     echo "========== Starting ComfyUI (Sage Attention) =========="
 	python3 -u main.py --use-sage-attention --listen --port 5555 ${CLI_ARGS}
 else
